b7efaf7fd4335739b4b05c310d6eff69996a4adb
mamorzik <upgcv@gmail.com>
2025-11-18 22:11:40 +0100

    Upd commit

--- Tutorium Projects/Tutorium 1/src/Main.java
+++ Tutorium Projects/Tutorium 1/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int smth = 5;
+        switch (smth) {
+            default:
+                System.out.println("0");
+                break;
+            case 5:
+                System.out.println("1");
+            case 2:
+                System.out.println("2");
+            case 1:
+                System.out.println("5");
+        }
     }
 }

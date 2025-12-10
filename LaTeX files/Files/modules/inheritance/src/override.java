fc2fd8f754456d19f5d8061b363c1d729e11bbd7
mamorzik <upgcv@gmail.com>
2025-12-10 13:38:35 +0100

    Added presentations for the whole 2025 year

--- LaTeX files/Files/modules/inheritance/src/override.java
+++ LaTeX files/Files/modules/inheritance/src/override.java
@@ -1,18 +1,18 @@
-public class @@cMutter@@ {
+public class Mutter {
    public void anstrengend(int k) {
       System.out.println(k + "Stueck Kuchen sind genug!");
    }
 }
 
-public class @@cOma@@ extends @@cMutter@@ {
-   @@c@Override@@
+public class Oma extends Mutter {
+   @Override
    public void anstrengend(int j) {
-      @@cSystem@@.out.println("Nimm doch noch " + j + " Muffins.");
+      System.out.println("Nimm doch noch " + j + " Muffins.");
    }
 }
 
-public class @@cUroma@@ extends @@cOma@@ {
-   @@c@Override@@
+public class Uroma extends Oma {
+   @Override
    public void anstrengend(int i) {
       System.out.println("Nimm doch noch "+ i +" Stueck Kuchen.");
    }
--- Tutorium Projects/DebuggerDemo/src/edu/kit/informatik/debugger/Loop.java
+++ Tutorium Projects/DebuggerDemo/src/edu/kit/informatik/debugger/Loop.java
@@ -1,5 +1,9 @@
 package edu.kit.informatik.debugger;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -8,8 +12,28 @@ public class Loop {
 
   public static final int MAX_NUMBER = (int) 1E5;
 
+  private void readFile(String path) {
+      try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+          String lineToCheck;
+          while ((lineToCheck = bufferedReader.readLine()) != null) {
+
+          }
+      } catch (IOException e) {
+          System.err.println("There was an exception with message: " + e.getMessage());
+      }
+
+
+//    try { // Environment where exceptions are treated, without instantly crashing the program
+//        readLines = Files.readAllLines(Path.of(path));
+//    } catch (IOException exception) { // If the specified exception is thrown, then go here
+//        System.err.println("Could not read file in " + path);
+//    }
+
+  }
+
+
   public static void main(String[] args) {
-    //What the heck does this do?
+//    What the heck does this do?
     List<Integer> intList = Stream.iterate(0, i -> i + 1)
             .limit(MAX_NUMBER)
             .toList();
@@ -19,5 +43,13 @@ public class Loop {
       // call helper method
       Recursion.fizzbuzz(i);
     }
+
+
+    System.out.println(new Loop().toString());
+  }
+
+  @Override
+  public String toString() {
+    return "My name is Aleks";
   }
 }

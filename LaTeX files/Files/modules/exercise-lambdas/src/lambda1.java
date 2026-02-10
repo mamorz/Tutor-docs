bc4b0abdf162aa689737baf1216c9bbc1e95d242
upgcv <upgcv@student.kit.edu>
2026-02-10 12:25:33 +0100

    11 Pres for Gr 12

---
+++ LaTeX files/Files/modules/exercise-lambdas/src/lambda1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Aufgabe {
+    public static void main(String[] args) {
+        List<String> cities = Arrays.asList("Berlin", "Ulm", "München", "Düsseldorf");
+
+        //Sortiere nach Länge des Strings (s1, s2).
+        Collections.sort(cities, _______);
+
+        System.out.println(cities);
+    }
+}
